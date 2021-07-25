1f2753cfe985e6a68c92bab991c16d08a8df6d7b
muhammad erlangga <erlanggamuhammad01@gmail.com>
2021-07-25 13:12:35 +0700

    feat: connect to mongodb and create get & create endpoint

---
+++ backend-node-express/model/cards.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+
+const cardSchema = mongoose.Schema(
+    {
+        name: String,
+        imgURL: String,
+    }
+)
+
+export default mongoose.model('cards', cardSchema)
